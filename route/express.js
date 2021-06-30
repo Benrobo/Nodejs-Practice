d52bb635c31136ce13eb9c3c6db89f703aade6fa
benrobo <alumonabenaiah71@gmail.com>
2021-06-30 17:00:56 +0100

    added express route

--- route/express.js
+++ route/express.js
@@ -1,13 +1,30 @@
 const express = require("express")
+const path = require("path")
 
+// create an express app
 const app = express()
 
+
+
+
+// create server
+const port =  5000
+
+// serving static files
+// app.use(express.static("views"))
+
+// adding prefix to the middleware function. we can create a virtual path prefix in the use() function which would lookup from the express.static() method, when we dont wanna let the user know that our files are been served from a folder called static
+
+// app.use("/static", express.static("views"))
+
+// or
+
+app.use("/static", express.static(path.join(__dirname, "views")))
+
+// if we dont wanna 
 app.get("/", (req, res)=>{
-    res.send("Home Page")
+    res.send("HOME PAGE")
 })
 
-app.post("/upload", (req, res)=>{
-    res.send("post route")
-})
 
-app.listen(5000)
+app.listen(port)
